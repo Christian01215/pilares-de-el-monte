b7193e815d1e050c8ecd43739f60e2ad78bde486
Christian Jesus Torres Jeldres <chjj01215@gmail.com>
2026-07-02 21:36:20 -0400

    Agrega WhiteNoise para corregir diseño del admin

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -51,6 +51,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'core.urls'
@@ -120,6 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Configuración de permisos CORS
 CORS_ALLOWED_ORIGINS = [
